d39c4b68802a31c4ccdcc19b76c3f43eb9fadea3
Harumi Kuno <harumi.kuno@hp.com>
2013-07-30 20:53:24 -0700

    Alistair's suggestions from code review.

--- tests/testutil.cmake
+++ tests/testutil.cmake
@@ -8,21 +8,28 @@ ENDFUNCTION()
 # Set "**/test-reports/*.xml" as the report file filter.
 
 # CMake function to add compile target, linking, ADD_TEST directives.
-FUNCTION (X_ADD_TESTCASE targetname the_libraries)
-    add_executable(${targetname} ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.cpp)
-    target_link_libraries(${targetname} ${the_libraries})
-    X_ADD_GTEST(${targetname})
+FUNCTION (X_ADD_TESTCASE targetname the_libraries )
+   if (argn)
+	foreach (f ${argn})
+            X_ADD_SM_TESTCASE(${targetname} ${the_libraries} ${f})
+        endforeach(f)
+    else(argn)
+       add_executable(${targetname} ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.cpp)
+       target_link_libraries(${targetname} ${the_libraries})
+       X_ADD_GTEST(${targetname})
+    endif(argn)
 ENDFUNCTION()
 
-# CMake function to add both swizzled and non-swizzled targets, linking, ADD_TEST directives.
+# CMake function to add compile target, linking, ADD_TEST directives.
+# Runs each target twice -- once without the special_compile_def and once with.
 FUNCTION (X_ADD_SM_TESTCASE targetname the_libraries special_compile_def)
     add_executable(${targetname} ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.cpp)
     target_link_libraries(${targetname} ${the_libraries})
     X_ADD_GTEST(${targetname})
-    set_property(TARGET ${targetname}  PROPERTY COMPILE_DEFINITIONS ${special_compile_def})
 
     add_executable(${targetname}_${special_compile_def} ${CMAKE_CURRENT_SOURCE_DIR}/${targetname}.cpp)
     target_link_libraries(${targetname}_${special_compile_def} ${the_libraries})
     X_ADD_GTEST(${targetname}_${special_compile_def})
+    set_property(TARGET ${targetname}_${special_compile_def}  PROPERTY COMPILE_DEFINITIONS ${special_compile_def})
 ENDFUNCTION()
 
