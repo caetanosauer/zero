92d99948f8985250a9d2bee78e97b8f4aff7de41
Harumi Kuno <harumi.kuno@hp.com>
2013-08-29 13:50:39 -0700

    Updated to 35000 records.

--- tests/sm/test_btree_durability.cpp
+++ tests/sm/test_btree_durability.cpp
@@ -43,8 +43,7 @@ w_rc_t dosome(ss_m* ssm, test_volume_t *test_volume) {
     // XXXX should move the following out to a separate DataGen class
     typedef unsigned int size_t;
     size_t const domain = 100000;
-    // size_t const records= 30000;  // this works, but takes longer 
-    size_t const records= 30000; 
+    size_t const records= 35000; 
     off_t  const logpagesize = 8192; // quantum of log file size
     off_t  logsize = 0; // log file size. Should grow monotonically.
     off_t  logsize2 = 0; // log file size. Should grow monotonically.
