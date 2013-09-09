ead9e2fc113cf2919480693a92a1e9ee956f3721
Mark Lillibridge <mark.lillibridge@hp.com>
2013-09-09 15:05:44 -0700

    adjust for moved checksum field

--- src/sm/generic_page.cpp
+++ src/sm/generic_page.cpp
@@ -7,19 +7,20 @@
 
 
 uint32_t generic_page_header::calculate_checksum () const {
+    // Region checksum can cover is [start..end):
+    const unsigned char *start = (const unsigned char *)this + sizeof(checksum);  // do not cover checksum field!
+    const unsigned char *end   = (const unsigned char *)this + page_sz;
+
+
+    // FIXME: The current checksum ignores most of the headers and
+    // data bytes, presumably for speed reasons.
+
     const uint32_t CHECKSUM_MULT = 0x35D0B891;
-
-    // FIXME: The current checksum ignores the headers and most of the
-    // data bytes, presumably for speed reasons.  If you start
-    // checksumming the headers, be careful of the checksum field.
-
-    const unsigned char *data      = (const unsigned char *)(this + 1);          // start of data section of this page: right after these headers
-    const unsigned char *data_last = (const unsigned char *)(this) + page_sz - sizeof(uint32_t);  // the last 32-bit word of the data section of this page
 
     uint64_t value = 0;
     // these values (23/511) are arbitrary
-    for (const unsigned char *p = (const unsigned char *) data + 23; p <= data_last; p += 511) {
-        // be aware of alignment issue on spark! so this code is not safe
+    for (const unsigned char *p = start + 23; p+3 < end; p += 511) {
+        // be aware of alignment issue on spark! so this code is not safe:
         // const uint32_t*next = reinterpret_cast<const uint32_t*>(p);
         // value ^= *next;
         value = value * CHECKSUM_MULT + p[0];
