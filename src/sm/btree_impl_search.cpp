da0015a7f55b997f9c6b36949696f43e84308be6
Mark Lillibridge <mark.lillibridge@hp.com>
2014-02-10 10:24:53 -0800

    code formatting only

--- src/sm/btree_impl_search.cpp
+++ src/sm/btree_impl_search.cpp
@@ -1,5 +1,5 @@
 /*
- * (c) Copyright 2011-2013, Hewlett-Packard Development Company, LP
+ * (c) Copyright 2011-2014, Hewlett-Packard Development Company, LP
  */
 
 #include "w_defines.h"
@@ -22,14 +22,8 @@
 #include "xct.h"
 
 rc_t
-btree_impl::_ux_lookup(
-    volid_t vol, snum_t store,
-    const w_keystr_t&       key,
-    bool&               found,
-    void*               el,
-    smsize_t&           elen
-    )        
-{
+btree_impl::_ux_lookup(volid_t vol, snum_t store, const w_keystr_t& key, bool& found,
+                       void* el, smsize_t& elen) {
     FUNC(btree_impl::_ux_lookup);
     INC_TSTAT(bt_find_cnt);
     while (true) {
@@ -39,25 +33,18 @@ btree_impl::_ux_lookup(
         }
         return rc;
     }
-    return RCOK;
 }
 
 rc_t
-btree_impl::_ux_lookup_core(
-    volid_t vol, snum_t store,
-    const w_keystr_t&   key,
-    bool&               found,
-    void*               el,
-    smsize_t&           elen
-    )        
-{
-    bool need_lock = g_xct_does_need_lock();
+btree_impl::_ux_lookup_core(volid_t vol, snum_t store, const w_keystr_t& key, 
+                            bool& found, void* el, smsize_t& elen) {
+    bool need_lock     = g_xct_does_need_lock();
     bool ex_for_select = g_xct_does_ex_lock_for_select();
 
-    btree_page_h                leaf; // first-leaf
+    btree_page_h leaf; // first-leaf
 
     // find the leaf (potentially) containing the key
-    W_DO( _ux_traverse(vol, store, key, t_fence_contain, LATCH_SH, leaf));
+    W_DO(_ux_traverse(vol, store, key, t_fence_contain, LATCH_SH, leaf));
 
     w_assert1(leaf.is_fixed());
     w_assert1(leaf.is_leaf());
@@ -75,7 +62,7 @@ btree_impl::_ux_lookup_core(
         return RCOK;
     }
 
-    // the key is found (though it might be a ghost)! let's get a lock.
+    // the key is found (though it might be a ghost)!  Let's get a lock.
     if (need_lock) {
         // only the key is locked (SN)
         W_DO (_ux_lock_key(leaf, key, LATCH_SH,
@@ -99,15 +86,9 @@ btree_impl::_ux_lookup_core(
 }
 
 rc_t
-btree_impl::_ux_traverse(
-    volid_t vol, snum_t store,
-    const w_keystr_t &key,
-    traverse_mode_t traverse_mode,
-    latch_mode_t leaf_latch_mode,
-    btree_page_h &leaf,
-    bool allow_retry
-    )
-{
+btree_impl::_ux_traverse(volid_t vol, snum_t store, const w_keystr_t &key,
+                         traverse_mode_t traverse_mode, latch_mode_t leaf_latch_mode,
+                         btree_page_h &leaf, bool allow_retry) {
     FUNC(btree_impl::_ux_traverse);
     INC_TSTAT(bt_traverse_cnt);
     if (key.is_posinf()) {
@@ -123,29 +104,28 @@ btree_impl::_ux_traverse(
     for (int times = 0; times < 20; ++times) { // arbitrary number
         inquery_verify_init(vol, store); // initialize in-query verification
         btree_page_h root_p;
-        w_assert1(!root_p.is_fixed());
         bool should_try_ex = (leaf_latch_mode == LATCH_EX &&
-            leaf_pid_causing_failed_upgrade == smlevel_0::bf->get_root_page_id(vol, store));
+                              leaf_pid_causing_failed_upgrade == smlevel_0::bf->get_root_page_id(vol, store));
         W_DO( root_p.fix_root(vol, store, should_try_ex ? LATCH_EX : LATCH_SH));
         w_assert1(root_p.is_fixed());
         
         if (root_p.get_foster() != 0) {
-            // root page has foster-child! let's grow the tree.
+            // root page has foster-child!  Let's grow the tree.
             if (root_p.latch_mode() != LATCH_EX) {
-                root_p.unfix(); // don't upgrade. re-fix.
+                root_p.unfix(); // don't upgrade.  Re-fix.
                 W_DO( root_p.fix_root(vol, store, LATCH_EX));
             }
             W_DO(_sx_grow_tree(root_p));
-            --times; // we don't penalize this. do it again.
+            --times; // We don't penalize this.  Do it again.
             continue;
         }
         
-        rc_t rc = _ux_traverse_recurse (root_p, key, traverse_mode, leaf_latch_mode, leaf, leaf_pid_causing_failed_upgrade);
-
+        rc_t rc = _ux_traverse_recurse (root_p, key, traverse_mode, leaf_latch_mode, leaf, 
+                                        leaf_pid_causing_failed_upgrade);
         if (rc.is_error()) {
             if (rc.err_num() == eGOODRETRY) {
                 // did some opportunistic structure modification, and going to retry
-                --times; // we don't penalize this. do it again.
+                --times; // we don't penalize this.  do it again.
                 continue;
             }
             if (rc.err_num() == eRETRY) {
@@ -169,15 +149,12 @@ btree_impl::_ux_traverse(
 }
 
 rc_t
-btree_impl::_ux_traverse_recurse(
-    btree_page_h&                        start,
-    const w_keystr_t&               key,
-    btree_impl::traverse_mode_t     traverse_mode,
-    latch_mode_t                    leaf_latch_mode,
-    btree_page_h&                        leaf,
-    shpid_t&                         leaf_pid_causing_failed_upgrade
-    )
-{
+btree_impl::_ux_traverse_recurse(btree_page_h&                start,
+                                 const w_keystr_t&            key,
+                                 btree_impl::traverse_mode_t  traverse_mode,
+                                 latch_mode_t                 leaf_latch_mode,
+                                 btree_page_h&                leaf,
+                                 shpid_t&                     leaf_pid_causing_failed_upgrade) {
     FUNC(btree_impl::_ux_traverse_recurse);
     INC_TSTAT(bt_partial_traverse_cnt);
 
@@ -194,14 +171,14 @@ btree_impl::_ux_traverse_recurse(
         if (do_inquery_verify) inquery_verify_fact(*current); // check the current page
 
         // use fence key to tell if this is the page
-        bool this_is_the_leaf_page = false;
-        slot_follow_t slot_to_follow = t_follow_invalid;
+        bool          this_is_the_leaf_page = false;
+        slot_follow_t slot_to_follow        = t_follow_invalid;
         _ux_traverse_search(traverse_mode, current, key,
                             this_is_the_leaf_page, slot_to_follow);
         
-        //Should be able to search that:
-        // (this_is_the_leaf_page && slot_to_follow==t_follow_invalid) ||
-        // (!this_is_the_leaf_page && slot_to_follow=!=t_follow_invalid)
+        // Should be able to search that:
+        //  (this_is_the_leaf_page && slot_to_follow==t_follow_invalid) ||
+        //  (!this_is_the_leaf_page && slot_to_follow=!=t_follow_invalid)
         
         if (this_is_the_leaf_page) {
             leaf = *current;
@@ -226,18 +203,18 @@ btree_impl::_ux_traverse_recurse(
         shpid_t pid_to_follow;
         shpid_t pid_to_follow_opaqueptr;
         if (slot_to_follow == t_follow_foster) {
-            pid_to_follow = current->get_foster();
+            pid_to_follow           = current->get_foster();
             pid_to_follow_opaqueptr = current->get_foster_opaqueptr();
-        } else if (slot_to_follow == t_follow_pid0) {
-            pid_to_follow = current->pid0();
+        } else if (slot_to_follow   == t_follow_pid0) {
+            pid_to_follow           = current->pid0();
             pid_to_follow_opaqueptr = current->pid0_opaqueptr();
         } else {
-            pid_to_follow = current->child(slot_to_follow);
+            pid_to_follow           = current->child(slot_to_follow);
             pid_to_follow_opaqueptr = current->child_opaqueptr(slot_to_follow);
         }    
         w_assert1(pid_to_follow);
 
-        // if worth it, do eager adoption. If it actually did something, it returns 
+        // if worth it, do eager adoption.  If it actually did something, it returns 
         // eGOODRETRY so that we will exit and retry
         if ((current->level() >= 3 || 
              (current->level() == 2 && slot_to_follow == t_follow_foster)) // next will be non-leaf
@@ -245,10 +222,9 @@ btree_impl::_ux_traverse_recurse(
             W_DO (_ux_traverse_try_eager_adopt(*current, pid_to_follow_opaqueptr));
         }
         
-        bool should_try_ex = leaf_latch_mode == LATCH_EX && (
-            pid_to_follow == leaf_pid_causing_failed_upgrade
-            || current->latch_mode() == LATCH_EX // we have EX latch; don't SH latch
-            );
+        bool should_try_ex = (leaf_latch_mode == LATCH_EX) &&
+                             (pid_to_follow == leaf_pid_causing_failed_upgrade
+                              || current->latch_mode() == LATCH_EX); // we have EX latch; don't SH latch
         
         if(current->level()==2 && slot_to_follow!=t_follow_foster && leaf_latch_mode==LATCH_EX) {
             //We're likely going to find the target next, so go ahead and EX if we need to.
@@ -262,9 +238,9 @@ btree_impl::_ux_traverse_recurse(
                                should_try_ex ? LATCH_EX : LATCH_SH));
         
         if (slot_to_follow != t_follow_foster && next->get_foster() != 0) {
-            // we followed a real-child pointer and found that it has foster..  let's adopt it! (but
-            // opportunistically).  just like eager adoption, it returns eGOODRETRY and we will
-            // retry if it succeeded. otherwise, go on.
+            // We followed a real-child pointer and found that it has foster... let's adopt it! (but
+            // opportunistically).  Just like eager adoption, it returns eGOODRETRY and we will
+            // retry if it succeeded.  otherwise, go on.
             W_DO(_ux_traverse_try_opportunistic_adopt(*current, *next));
         }
 
@@ -286,7 +262,7 @@ void btree_impl::_ux_traverse_search(btree_impl::traverse_mode_t traverse_mode,
             if (current->is_leaf()) {
                 this_is_the_leaf_page = true;
             } else {
-                // this is an interior node. find a child to follow
+                // this is an interior node.  find a child to follow
                 slotid_t slot;
                 current->search_node(key, slot);
                 if (slot < 0) {
@@ -346,19 +322,18 @@ void btree_impl::_ux_traverse_search(btree_impl::traverse_mode_t traverse_mode,
             // let's follow foster
             slot_to_follow = t_follow_foster;
         } else {
-            // key is lower than fence-high. then one of the children must have it.
+            // key is lower than fence-high.  then one of the children must have it.
             w_assert2(!current->is_leaf()); // otherwise we should have seen an exact match
 
-            // We use search instead of search_node here because we
-            // need slightly different behavior:
+            // We use search instead of search_node here because we need slightly
+            // different behavior:
             //
-            // search_node returns the slot whose child pointer we
-            // should follow while searching for key.  For example, a
-            // separator key "AB" sends "AA" to left, "AAZ" to the
-            // left, "AB" to the right, "ABA" to the right, and "AC"
-            // to the right.
-            // However, now we are looking for fence-high key match,
-            // so we want to send "AB" to the left.
+            // search_node returns the slot whose child pointer we should follow while
+            // searching for key.  For example, a separator key "AB" sends "AA" to left,
+            // "AAZ" to the left, "AB" to the right, "ABA" to the right, and "AC" to the
+            // right.
+            // However, now we are looking for fence-high key match, so we want to send
+            // "AB" to the left.
             bool     key_found;
             slotid_t slot;
             current->search(key, key_found, slot);
@@ -367,16 +342,15 @@ void btree_impl::_ux_traverse_search(btree_impl::traverse_mode_t traverse_mode,
         }
     }
 } 
-rc_t btree_impl::_ux_traverse_try_eager_adopt(btree_page_h &current, shpid_t next_pid)
-{
+rc_t btree_impl::_ux_traverse_try_eager_adopt(btree_page_h &current, shpid_t next_pid) {
     w_assert1(current.is_fixed());
     queue_based_lock_t *mutex = mutex_for_high_contention(next_pid);
     w_assert1(mutex);
     {
         CRITICAL_SECTION(cs, *mutex);
         if (!is_ex_recommended(next_pid)) {
-            // probably some other thread has already done it. so just do nothing.
-            // even if this is unluckily some accident, that's fine. adoption can be delayed.
+            // probably some other thread has already done it.  so just do nothing.
+            // even if this is unluckily some accident, that's fine.  adoption can be delayed.
             // Also, we should exit critical section as soon as possible!
             // and just go on (return RCOK, not eGOODRETRY) with SH latch
             return RCOK;
@@ -389,20 +363,19 @@ rc_t btree_impl::_ux_traverse_try_eager_adopt(btree_page_h &current, shpid_t nex
             // same as above
             return RCOK;
         } else {
-            // this page has been requested for adoption many times. Let's do it!
+            // this page has been requested for adoption many times.  Let's do it!
             W_DO(_sx_adopt_foster_sweep_approximate(next, 0));
         }
     }
-    return RC(eGOODRETRY); // to be safe, let's restart. this is anyway rare event
+    return RC(eGOODRETRY); // to be safe, let's restart.  this is anyway rare event
 }
-rc_t btree_impl::_ux_traverse_try_opportunistic_adopt(btree_page_h &current, btree_page_h &next)
-{
+rc_t btree_impl::_ux_traverse_try_opportunistic_adopt(btree_page_h &current, btree_page_h &next) {
     w_assert1(current.is_fixed());
     w_assert1(next.is_fixed());
     bool pushedup;
     W_DO(_sx_opportunistic_adopt_foster(current, next, pushedup));
     // if it's pushed up, we restart the search from root
-    // (we can keep searching with a bit complicated code.. but wouldn't worth it)
+    // (we can keep searching with a bit complicated code..  but wouldn't worth it)
     if (pushedup) {
         return RC(eGOODRETRY);
     } else {
