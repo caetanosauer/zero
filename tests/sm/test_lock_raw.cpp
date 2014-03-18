6d9d640546b8b694ea0d16719556151b5b2d5abb
Hideaki Kimura <hideaki.kimura@hp.com>
2014-03-18 16:44:17 -0700

    added testcase

--- tests/sm/test_lock_raw.cpp
+++ tests/sm/test_lock_raw.cpp
@@ -10,6 +10,7 @@
 #include "log_lsn_tracker.h"
 #include "lock.h"
 #include "w_okvl_inl.h"
+#include "w_endian.h"
 #include "../fc/local_random.h"
 
 sm_options make_options(bool has_init = true, bool small = true) {
@@ -205,6 +206,99 @@ TEST (LockRawTest, ParallelSsmInvolved) {
 TEST (LockRawTest, ParallelSsmInvolvedCatchup) {
     test_env->empty_logdata_dir();
     EXPECT_EQ(test_env->runBtreeTest(parallel_locks, true, 4096, make_options_huge(true)), 0);
+}
+
+
+uint64_t next_history = 0;
+#if W_DEBUG_LEVEL>0
+const int APPEND_COUNT = 1000;
+#else // W_DEBUG_LEVEL>0
+const int APPEND_COUNT = 10000;
+#endif // W_DEBUG_LEVEL>0
+
+class append_worker_t : public smthread_t {
+public:
+    append_worker_t()
+        : smthread_t(t_regular, "append_worker_t"), _running(true) {
+        _thid = next_thid++;
+    }
+    virtual void run() {
+        tlr_t rand (_thid);
+        std::cout << "Worker-" << _thid << " started" << std::endl;
+        _rc = x_begin_xct(ssm, true);
+        EXPECT_FALSE(_rc.is_error());
+        w_keystr_t key;
+        uint64_t key_int = 0;
+        char key_be[8];
+
+        char data[1]; // whatever
+        data[0] = 0;
+        vec_t vec(data, 1);
+
+        for (int i = 0; i < APPEND_COUNT; ++i) {
+            if (i % 1000 == 0) {
+                std::cout << "Worker-" << _thid << " " << i << "/" << APPEND_COUNT << std::endl;
+            }
+            if (i % 10 == 0) {
+                if (i % 100 == 0) {
+                    _rc = ssm->abort_xct();
+                    EXPECT_FALSE(_rc.is_error());
+                } else {
+                    _rc = ssm->commit_xct(true);
+                    EXPECT_FALSE(_rc.is_error());
+                }
+
+                _rc = x_begin_xct(ssm, true);
+                EXPECT_FALSE(_rc.is_error());
+            }
+            key_int = lintel::unsafe::atomic_fetch_add<uint64_t>(&next_history, 1);
+            serialize64_be(key_be, key_int);
+            key.construct_regularkey(key_be, sizeof(key_int));
+            _rc = ssm->create_assoc(stid, key, vec);
+            EXPECT_FALSE(_rc.is_error()) << "key=" << key_int << ": rc=" << _rc;
+        }
+        _rc = x_commit_xct(ssm);
+        EXPECT_FALSE(_rc.is_error());
+        _running = false;
+        std::cout << "done:" << _thid << std::endl;
+    }
+    int  return_value() const { return 0; }
+    rc_t _rc;
+    bool _running;
+    int _thid;
+    ss_m *ssm;
+    test_volume_t *volume;
+    stid_t stid;
+    lpid_t root_pid;
+};
+
+w_rc_t parallel_appends(ss_m *ssm, test_volume_t *volume) {
+    stid_t stid;
+    lpid_t root_pid;
+    W_DO(x_btree_create_index(ssm, volume, stid, root_pid));
+
+    append_worker_t workers[THREAD_COUNT];
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        workers[i].ssm = ssm;
+        workers[i].volume = volume;
+        workers[i].stid = stid;
+        workers[i].root_pid = root_pid;
+        W_DO(workers[i].fork());
+    }
+
+    for (int i = 0; i < THREAD_COUNT; ++i) {
+        W_DO(workers[i].join());
+        EXPECT_FALSE(workers[i]._running) << i;
+        EXPECT_FALSE(workers[i]._rc.is_error()) << i;
+    }
+    return RCOK;
+}
+TEST (LockRawTest, ParallelAppends) {
+    test_env->empty_logdata_dir();
+    sm_options options = make_options_huge(false);
+    options.set_int_option("sm_logbufsize", 128 << 10);
+    options.set_int_option("sm_logsize", 8192 << 10);
+    EXPECT_EQ(test_env->runBtreeTest(parallel_appends, true, 1 << 16, options), 0);
 }
 
 int main(int argc, char **argv) {
