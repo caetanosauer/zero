73d910015573894beff4260582c447bc91a9398e
Caetano Sauer <csauer@cs.uni-kl.de>
2015-04-14 16:51:28 +0200

    Replaced tab with spaces in logfactory code

--- tests/sm/logfactory.cpp
+++ tests/sm/logfactory.cpp
@@ -4,10 +4,10 @@ const uint32_t LogFactory::factory_version_major = 6;
 const uint32_t LogFactory::factory_version_minor = 1;
 
 /**
- *	params:
- *		max_page_id: self-explained
- *		th: increase max_page_id after th records were generated
- *		ratio: increase max_page_id by ratio (max_page_id =* ratio)
+ *    params:
+ *        max_page_id: self-explained
+ *        th: increase max_page_id after th records were generated
+ *        ratio: increase max_page_id by ratio (max_page_id =* ratio)
  */
 LogFactory::LogFactory(bool sorted, unsigned max_page_id, unsigned th,
         unsigned increment)
@@ -16,60 +16,60 @@ LogFactory::LogFactory(bool sorted, unsigned max_page_id, unsigned th,
     max_page_id(max_page_id), prev_lsn(max_page_id, lsn_t::null),
     generatedCount(0), gen(1729), dDist(0.0,1.0)
 {
-	if (factory_version_major != log_storage::_version_major 
-		|| 
-		factory_version_minor > log_storage::_version_minor) {
-		DBGTHRD(<< "FACTORY VERSION ERROR");
-	}
-	if (Stats::stats_version_major != factory_version_major
-		|| 
-		Stats::stats_version_minor > factory_version_minor) {
-		DBGTHRD(<< "STATS VERSION ERROR");
-	}
+    if (factory_version_major != log_storage::_version_major 
+        || 
+        factory_version_minor > log_storage::_version_minor) {
+        DBGTHRD(<< "FACTORY VERSION ERROR");
+    }
+    if (Stats::stats_version_major != factory_version_major
+        || 
+        Stats::stats_version_minor > factory_version_minor) {
+        DBGTHRD(<< "STATS VERSION ERROR");
+    }
 }
 
 LogFactory::~LogFactory() {
 }
 
 bool LogFactory::next(void* addr) {
-	fake_logrec_t* lr = new (addr) fake_logrec_t;
+    fake_logrec_t* lr = new (addr) fake_logrec_t;
 
-	lr->header._type = stats.nextType();
-	lr->header._len = stats.nextLength(lr->header._type);
-	lr->header._cat = fake_logrec_t::t_status;
-	lr->header._shpid = nextZipf();
-	lr->header._vid = 1;	// volume id
-	lr->header._page_tag = 1;
-	lr->header._snum = 1;	// storage number
-	lr->header._page_prv = prev_lsn[lr->header._shpid];
+    lr->header._type = stats.nextType();
+    lr->header._len = stats.nextLength(lr->header._type);
+    lr->header._cat = fake_logrec_t::t_status;
+    lr->header._shpid = nextZipf();
+    lr->header._vid = 1;    // volume id
+    lr->header._page_tag = 1;
+    lr->header._snum = 1;    // storage number
+    lr->header._page_prv = prev_lsn[lr->header._shpid];
 
-	lr->xidInfo._xid = 1;	// transaction id
-	lr->xidInfo._xid_prv = lsn_t::null;
+    lr->xidInfo._xid = 1;    // transaction id
+    lr->xidInfo._xid_prv = lsn_t::null;
 
     // TODO: do we need to differentiate between SSX and non-SSX logrecs?
-	/* No. The only difference is that SSX records do not have the xidInfo
-	 * structure. Since the LogFactory is not interested in these fields, we
-	 * can treat SSX as normal log records. */
+    /* No. The only difference is that SSX records do not have the xidInfo
+     * structure. Since the LogFactory is not interested in these fields, we
+     * can treat SSX as normal log records. */
 
-	/* Fill the data portion of the log record with 6s (luck number) */
-	memset(lr->_data, 6, lr->header._len - (logrec_t::hdr_non_ssx_sz + sizeof(lsn_t)));
+    /* Fill the data portion of the log record with 6s (luck number) */
+    memset(lr->_data, 6, lr->header._len - (logrec_t::hdr_non_ssx_sz + sizeof(lsn_t)));
 
-	/* Check if currentLSN will overflow */
-	if((nextLSN.rba()+lr->header._len) > lsn_t::max.rba()) {
-		nextLSN = lsn_t(nextLSN.file()+1, 0);
-	}
+    /* Check if currentLSN will overflow */
+    if((nextLSN.rba()+lr->header._len) > lsn_t::max.rba()) {
+        nextLSN = lsn_t(nextLSN.file()+1, 0);
+    }
 
-	lr->set_lsn_ck(nextLSN); // set lsn in the last 8 bytes
-	prev_lsn[lr->header._shpid] = lr->get_lsn_ck();
-	nextLSN += lr->header._len;
+    lr->set_lsn_ck(nextLSN); // set lsn in the last 8 bytes
+    prev_lsn[lr->header._shpid] = lr->get_lsn_ck();
+    nextLSN += lr->header._len;
 
-	generatedCount++;
+    generatedCount++;
 
-	//DBGTHRD(<< "New log record created: ["
+    //DBGTHRD(<< "New log record created: ["
                 //<< ((logrec_t*)r)->type_str() << ", "
-		//<< r->_len << "," << r->_shpid << ","
+        //<< r->_len << "," << r->_shpid << ","
                 //<< r->_prev_page << "," << *(r->_lsn_ck()) << "]");
-	return true;        
+    return true;        
 }
 
 /**
@@ -77,17 +77,17 @@ bool LogFactory::next(void* addr) {
  * Generate page_id in the range [0, max_page_id] (at least I hope so).
  */
 unsigned LogFactory::nextZipf() {
-	if(generatedCount % INCR_TH == 0) {
-		max_page_id += INCR_RATIO;
+    if(generatedCount % INCR_TH == 0) {
+        max_page_id += INCR_RATIO;
         prev_lsn.resize(max_page_id, lsn_t::null);
-	}
+    }
 
     if (sorted) {
         return max_page_id;
     }
 
-	double h = 0.2;
-	int r = (int) ((max_page_id+1) * pow(dDist(gen), (log(h)/log(1-h))));
+    double h = 0.2;
+    int r = (int) ((max_page_id+1) * pow(dDist(gen), (log(h)/log(1-h))));
 
-	return max_page_id - r; /* Try to favor higher page ids */
+    return max_page_id - r; /* Try to favor higher page ids */
 }
--- tests/sm/logfactory.h
+++ tests/sm/logfactory.h
@@ -13,149 +13,149 @@
 
 class LogFactory {
 public:
-	/* max_page_id = 233220, similar to a TPCC with SF=10 (~2GB with 8KB pages).
-	 * th(increase_threshold) = increase database every 1000 log records generated.
-	 * ratio = increment on number of pages at the threshold above
-	 */
-	LogFactory(
+    /* max_page_id = 233220, similar to a TPCC with SF=10 (~2GB with 8KB pages).
+     * th(increase_threshold) = increase database every 1000 log records generated.
+     * ratio = increment on number of pages at the threshold above
+     */
+    LogFactory(
                 bool sorted = false, // generates sorted log archive
                 unsigned max_page_id = 233220,
                 unsigned th = 100,
                 unsigned increment = 10
         );
-	~LogFactory();
+    ~LogFactory();
 
-	//void open(lsn_t endLSN); SHOULD THIS BE IMPLEMENTED?
+    //void open(lsn_t endLSN); SHOULD THIS BE IMPLEMENTED?
         bool next(void* addr);
         lsn_t getNextLSN() { return nextLSN; }
 
 private:
-	/* fake_logrec_t used to have access to private members of logrec_t.
-	 */
-	class shore_fake_logrec_t {
-	public:
-		enum {
-			max_sz = 3 * sizeof(generic_page),
-			hdr_sz = (
-				sizeof(uint2_t) +   // _len
-				sizeof(u_char) +    // _type
-				sizeof(u_char) +    //  _cat
-				sizeof(tid_t) +     // _tid
-				sizeof(shpid_t) +   // _shpid
-				sizeof(vid_t) +     // _vid
-				sizeof(uint2_t) +   // _page_tag
-				sizeof(snum_t) +    // _snum
-				sizeof(lsn_t) +     // _prev // ctns possibly 4 extra
-				sizeof(lsn_t) +     // _prev_page
-				0
-			)
-		};
+    /* fake_logrec_t used to have access to private members of logrec_t.
+     */
+    class shore_fake_logrec_t {
+    public:
+        enum {
+            max_sz = 3 * sizeof(generic_page),
+            hdr_sz = (
+                sizeof(uint2_t) +   // _len
+                sizeof(u_char) +    // _type
+                sizeof(u_char) +    //  _cat
+                sizeof(tid_t) +     // _tid
+                sizeof(shpid_t) +   // _shpid
+                sizeof(vid_t) +     // _vid
+                sizeof(uint2_t) +   // _page_tag
+                sizeof(snum_t) +    // _snum
+                sizeof(lsn_t) +     // _prev // ctns possibly 4 extra
+                sizeof(lsn_t) +     // _prev_page
+                0
+            )
+        };
 
-		enum {
-			data_sz = max_sz - (hdr_sz + sizeof(lsn_t))
-		};
+        enum {
+            data_sz = max_sz - (hdr_sz + sizeof(lsn_t))
+        };
 
-		uint2_t		_len;
-		u_char      _type;
-		u_char      _cat;
-		shpid_t     _shpid;
-		tid_t       _tid;
-		vid_t       _vid;
-		uint2_t     _page_tag;
-		snum_t      _snum;
-		lsn_t       _prev;
-		lsn_t       _prev_page;
-		char       _data[data_sz + sizeof(lsn_t)];
+        uint2_t        _len;
+        u_char      _type;
+        u_char      _cat;
+        shpid_t     _shpid;
+        tid_t       _tid;
+        vid_t       _vid;
+        uint2_t     _page_tag;
+        snum_t      _snum;
+        lsn_t       _prev;
+        lsn_t       _prev_page;
+        char       _data[data_sz + sizeof(lsn_t)];
 
-		const lsn_t     get_lsn_ck() const { 
-                        	lsn_t    tmp = *_lsn_ck();
+        const lsn_t     get_lsn_ck() const { 
+                            lsn_t    tmp = *_lsn_ck();
                             return tmp;
                         }
-    	void            set_lsn_ck(const lsn_t &lsn_ck) {
+        void            set_lsn_ck(const lsn_t &lsn_ck) {
                             lsn_t& where = *_lsn_ck();
                             where = lsn_ck;
                         }
     private:
-    	lsn_t*          _lsn_ck() const {
-        					size_t lo_offset = _len - (hdr_sz + sizeof(lsn_t));
-        					w_assert3(alignon(_data+lo_offset, 8));
-        					lsn_t *where = (lsn_t*)(_data+lo_offset);
-        					return where;
-						}
-	};
+        lsn_t*          _lsn_ck() const {
+                            size_t lo_offset = _len - (hdr_sz + sizeof(lsn_t));
+                            w_assert3(alignon(_data+lo_offset, 8));
+                            lsn_t *where = (lsn_t*)(_data+lo_offset);
+                            return where;
+                        }
+    };
 
-	class fake_logrec_t {
-	public:
-		#include "logtype_gen.h"
-	    enum {
-	        max_sz = 3 * sizeof(generic_page),
-	        hdr_non_ssx_sz = sizeof(baseLogHeader) + sizeof(xidChainLogHeader),
-	        hdr_single_sys_xct_sz = sizeof(baseLogHeader),
-	        max_data_sz = max_sz - hdr_non_ssx_sz - sizeof(lsn_t)
-	    };
+    class fake_logrec_t {
+    public:
+        #include "logtype_gen.h"
+        enum {
+            max_sz = 3 * sizeof(generic_page),
+            hdr_non_ssx_sz = sizeof(baseLogHeader) + sizeof(xidChainLogHeader),
+            hdr_single_sys_xct_sz = sizeof(baseLogHeader),
+            max_data_sz = max_sz - hdr_non_ssx_sz - sizeof(lsn_t)
+        };
 
-	    BOOST_STATIC_ASSERT(hdr_non_ssx_sz == 40);
-	    BOOST_STATIC_ASSERT(hdr_single_sys_xct_sz == 40 - 16);
+        BOOST_STATIC_ASSERT(hdr_non_ssx_sz == 40);
+        BOOST_STATIC_ASSERT(hdr_single_sys_xct_sz == 40 - 16);
 
-	    enum category_t {
-	    t_bad_cat   = 0x00,
-	    t_status    = 0x01,
-	    t_undo      = 0x02,
-	    t_redo      = 0x04,
-	    t_multi     = 0x08,
-	    t_logical   = 0x10,
-	    t_cpsn      = 0x20,
-	    t_rollback  = 0x40,
-	    t_single_sys_xct    = 0x80
-	    };
+        enum category_t {
+        t_bad_cat   = 0x00,
+        t_status    = 0x01,
+        t_undo      = 0x02,
+        t_redo      = 0x04,
+        t_multi     = 0x08,
+        t_logical   = 0x10,
+        t_cpsn      = 0x20,
+        t_rollback  = 0x40,
+        t_single_sys_xct    = 0x80
+        };
 
-	    baseLogHeader header;
-	    xidChainLogHeader xidInfo;
-	    char _data[max_sz - sizeof(baseLogHeader) - sizeof(xidChainLogHeader)];
+        baseLogHeader header;
+        xidChainLogHeader xidInfo;
+        char _data[max_sz - sizeof(baseLogHeader) - sizeof(xidChainLogHeader)];
 
-	    const lsn_t          get_lsn_ck() const { 
-	                                lsn_t    tmp = *_lsn_ck();
-	                                return tmp;
-	                         }
-	    void                 set_lsn_ck(const lsn_t &lsn_ck) {
-	                                // put lsn in last bytes of data
-	                                lsn_t& where = *_lsn_ck();
-	                                where = lsn_ck;
-	                         }
+        const lsn_t          get_lsn_ck() const { 
+                                    lsn_t    tmp = *_lsn_ck();
+                                    return tmp;
+                             }
+        void                 set_lsn_ck(const lsn_t &lsn_ck) {
+                                    // put lsn in last bytes of data
+                                    lsn_t& where = *_lsn_ck();
+                                    where = lsn_ck;
+                             }
 
-	private:
-		lsn_t*            _lsn_ck() {
-	        w_assert3(alignon(header._len, 8));
-	        char* this_ptr = reinterpret_cast<char*>(this);
-	        return reinterpret_cast<lsn_t*>(this_ptr + header._len - sizeof(lsn_t));
-	    }
-	    const lsn_t*            _lsn_ck() const {
-	        w_assert3(alignon(header._len, 8));
-	        const char* this_ptr = reinterpret_cast<const char*>(this);
-	        return reinterpret_cast<const lsn_t*>(this_ptr + header._len - sizeof(lsn_t));
-	    }	    
-	};
+    private:
+        lsn_t*            _lsn_ck() {
+            w_assert3(alignon(header._len, 8));
+            char* this_ptr = reinterpret_cast<char*>(this);
+            return reinterpret_cast<lsn_t*>(this_ptr + header._len - sizeof(lsn_t));
+        }
+        const lsn_t*            _lsn_ck() const {
+            w_assert3(alignon(header._len, 8));
+            const char* this_ptr = reinterpret_cast<const char*>(this);
+            return reinterpret_cast<const lsn_t*>(this_ptr + header._len - sizeof(lsn_t));
+        }        
+    };
 
-	Stats stats;
+    Stats stats;
 
-	/* Every INCR_TH records generated, increase the max_page_id by a ratio 
-	 * of INCR_RATIO.
-	 */
-	const unsigned INCR_TH;
-	const unsigned INCR_RATIO;
+    /* Every INCR_TH records generated, increase the max_page_id by a ratio 
+     * of INCR_RATIO.
+     */
+    const unsigned INCR_TH;
+    const unsigned INCR_RATIO;
 
     bool sorted;
-	unsigned max_page_id;
-	vector<lsn_t> prev_lsn;	/* Keep track of previous log record on same page */
-	uint4_t generatedCount;	/* Keep track of how many log records were generated */
-	lsn_t nextLSN;
+    unsigned max_page_id;
+    vector<lsn_t> prev_lsn;    /* Keep track of previous log record on same page */
+    uint4_t generatedCount;    /* Keep track of how many log records were generated */
+    lsn_t nextLSN;
 
-	boost::random::mt19937 gen;	/* Random Number Generator */
-	boost::random::uniform_real_distribution<double> dDist; //[min,max)
+    boost::random::mt19937 gen;    /* Random Number Generator */
+    boost::random::uniform_real_distribution<double> dDist; //[min,max)
 
-	unsigned nextZipf();
+    unsigned nextZipf();
 
-	static const uint32_t  factory_version_major;
+    static const uint32_t  factory_version_major;
     static const uint32_t  factory_version_minor;
 
 };
