475c2012c77fadfa25d529a597fc3a036a621bda
Caetano Sauer <csauer@cs.uni-kl.de>
2015-04-15 15:04:07 +0200

    Updated Log Archiver test with RunMerger test cases
    - One "sequential" test uses runs generated by LogFactory, which are
    strictly increasing in both PID and LSN order. Thus, all log records of
    run n are produced before all of n+1 during merge. This allows to catch
    many bugs, but is not realistic.
    - The "full" test populates a B-tree and generates an archive using the
    complete LogArchiver infrastructure. Then it consumes the merged input
    from all produced runs. This is a realistic test case.

--- tests/sm/test_logarchiver.cpp
+++ tests/sm/test_logarchiver.cpp
@@ -93,6 +93,50 @@ rc_t generateFakeArchive(unsigned bytesPerRun, unsigned runCount,
     return RCOK;
 }
 
+rc_t emptyHeapAndCheck(LogArchiver::ArchiverHeap& heap)
+{
+    logrec_t* lr;
+    shpid_t prevPage = (shpid_t) 0;
+    lsn_t prevLSN = lsn_t::null;
+    while (heap.size() > 0) {
+        lr = heap.top();
+        EXPECT_TRUE(lr->lsn_ck() != prevLSN);
+        EXPECT_TRUE(lr->construct_pid().page >= prevPage);
+        if (lr->construct_pid().page == prevPage) {
+            EXPECT_TRUE(lr->lsn_ck() > prevLSN);
+        }
+        prevPage = lr->construct_pid().page;
+        prevLSN = lr->lsn_ck();
+        heap.pop();
+    }
+
+    return RCOK;
+}
+
+LogArchiver::ArchiveScanner::RunMerger*
+buildRunMergerFromDirectory(LogArchiver::ArchiveDirectory& dir)
+{
+    std::vector<string> files;
+    W_COERCE(dir.listFiles(&files));
+    LogArchiver::ArchiveScanner::RunMerger* merger =
+        new LogArchiver::ArchiveScanner::RunMerger();
+
+    for (size_t i = 0; i < files.size(); i++) {
+        lsn_t beginLSN = LogArchiver::ArchiveDirectory::parseLSN(
+                files[i].c_str(), false);
+        lsn_t endLSN = LogArchiver::ArchiveDirectory::parseLSN(
+                files[i].c_str(), true);
+
+        LogArchiver::ArchiveScanner::RunScanner* rs =
+            new LogArchiver::ArchiveScanner::RunScanner(
+                beginLSN, endLSN, lpid_t::null, lpid_t::null, 0, &dir);
+
+        merger->addInput(rs);
+    }
+
+    return merger;
+}
+
 /******************************************************************************
  * TESTS
  */
@@ -124,26 +168,6 @@ rc_t consumerTest(ss_m* ssm, test_volume_t* test_vol)
     return RCOK;
 }
 
-rc_t checkHeapOrder(LogArchiver::ArchiverHeap& heap)
-{
-    logrec_t* lr;
-    shpid_t prevPage = (shpid_t) 0;
-    lsn_t prevLSN = lsn_t::null;
-    while (heap.size() > 0) {
-        lr = heap.top();
-        std::cout << *lr << std::endl;
-        EXPECT_TRUE(lr->construct_pid().page >= prevPage);
-        if (lr->construct_pid().page == prevPage) {
-            EXPECT_TRUE(lr->lsn_ck() > prevLSN);
-        }
-        prevPage = lr->construct_pid().page;
-        prevLSN = lr->lsn_ck();
-        heap.pop();
-    }
-
-    return RCOK;
-}
-
 // TODO implement heapTestFactory that uses LogFactory
 rc_t heapTestReal(ss_m* ssm, test_volume_t* test_vol)
 {
@@ -162,13 +186,13 @@ rc_t heapTestReal(ss_m* ssm, test_volume_t* test_vol)
     while (cons.next(lr)) {
         pushed = heap.push(lr);
         if (!pushed) {
-            checkHeapOrder(heap);
+            emptyHeapAndCheck(heap);
             pushed = heap.push(lr);
             EXPECT_TRUE(pushed);
         }
     }
 
-    checkHeapOrder(heap);
+    emptyHeapAndCheck(heap);
     EXPECT_EQ(0, heap.size());
 
     return RCOK;
@@ -209,6 +233,7 @@ rc_t runScannerTest(ss_m* /* ssm */, test_volume_t* /* test_vol */)
     unsigned total = 0;
     generateFakeArchive(BLOCK_SIZE*8, 1, false, total);
 
+    // TODO: runScanner does not work with index because index blocks are read at the end
     LogArchiver::ArchiveDirectory dir(test_env->archive_dir, BLOCK_SIZE,
             false /* createIndex */);
     EXPECT_EQ(NULL, dir.getIndex());
@@ -217,7 +242,6 @@ rc_t runScannerTest(ss_m* /* ssm */, test_volume_t* /* test_vol */)
 
     EXPECT_EQ(1, files.size());
 
-    std::string fname = dir.getArchDir() + files[0];
     lsn_t beginLSN = LogArchiver::ArchiveDirectory::parseLSN(files[0].c_str(),
             false);
     lsn_t endLSN = LogArchiver::ArchiveDirectory::parseLSN(files[0].c_str(),
@@ -227,16 +251,88 @@ rc_t runScannerTest(ss_m* /* ssm */, test_volume_t* /* test_vol */)
         (beginLSN, endLSN, lpid_t::null, lpid_t::null, 0, &dir);
 
     lpid_t prevPID = lpid_t::null;
+    lsn_t prevLSN = lsn_t::null;
     unsigned count = 0;
     logrec_t* lr;
     while (rs.next(lr)) {
         EXPECT_TRUE(lr->valid_header(lr->lsn_ck()));
         EXPECT_TRUE(lr->construct_pid().page >= prevPID.page);
+        EXPECT_TRUE(lr->lsn_ck() != prevLSN);
+        EXPECT_TRUE(lr->lsn_ck() >= rs.runBegin);
+        EXPECT_TRUE(lr->lsn_ck() < rs.runEnd);
         prevPID = lr->construct_pid();
+        prevLSN = lr->lsn_ck();
         count++;
     }
 
     EXPECT_EQ(total, count);
+
+    return RCOK;
+}
+
+rc_t runMergerSeqTest(ss_m* /* ssm */, test_volume_t* /* test_vol */)
+{
+    unsigned total = 0;
+    generateFakeArchive(BLOCK_SIZE*8, 8, false, total);
+
+    LogArchiver::ArchiveDirectory dir(test_env->archive_dir, BLOCK_SIZE,
+            false /* createIndex */);
+    EXPECT_EQ(NULL, dir.getIndex());
+
+
+    LogArchiver::ArchiveScanner::RunMerger* merger =
+        buildRunMergerFromDirectory(dir);
+
+    lpid_t prevPID = lpid_t::null;
+    lsn_t prevLSN = lsn_t::null;
+    unsigned count = 0;
+    logrec_t* lr;
+    while (merger->next(lr)) {
+        EXPECT_TRUE(lr->valid_header(lr->lsn_ck()));
+        EXPECT_TRUE(lr->construct_pid().page >= prevPID.page);
+        EXPECT_TRUE(lr->lsn_ck() != prevLSN);
+        prevPID = lr->construct_pid();
+        prevLSN = lr->lsn_ck();
+        count++;
+    }
+
+    EXPECT_EQ(total, count);
+
+    return RCOK;
+}
+
+rc_t runMergerFullTest(ss_m* ssm, test_volume_t* test_vol)
+{
+    unsigned howManyToInsert = 2000;
+    W_DO(populateBtree(ssm, test_vol, howManyToInsert));
+
+    LogArchiver::LogConsumer cons(lsn_t(1,0), BLOCK_SIZE);
+    LogArchiver::ArchiverHeap heap(BLOCK_SIZE);
+    LogArchiver::ArchiveDirectory dir(test_env->archive_dir, BLOCK_SIZE, false);
+    LogArchiver::BlockAssembly assemb(&dir);
+
+    LogArchiver la(&dir, &cons, &heap, &assemb);
+    la.fork();
+    la.activate(lsn_t::null, true /* wait */);
+    // wait for logarchiver to consume up to durable LSN,
+    la.activate(lsn_t::null, true);
+
+    LogArchiver::ArchiveScanner::RunMerger* merger =
+        buildRunMergerFromDirectory(dir);
+
+    lpid_t prevPID = lpid_t::null;
+    lsn_t prevLSN = lsn_t::null;
+    logrec_t* lr;
+    while (merger->next(lr)) {
+        EXPECT_TRUE(lr->valid_header(lr->lsn_ck()));
+        EXPECT_TRUE(lr->construct_pid().page >= prevPID.page);
+        EXPECT_TRUE(lr->lsn_ck() != prevLSN);
+        prevPID = lr->construct_pid();
+        prevLSN = lr->lsn_ck();
+    }
+
+    la.start_shutdown();
+    la.join();
 
     return RCOK;
 }
@@ -248,10 +344,12 @@ rc_t runScannerTest(ss_m* /* ssm */, test_volume_t* /* test_vol */)
         EXPECT_EQ(test_env->runBtreeTest(function), 0); \
     }
 
-//DEFAULT_TEST (LogArchiverTest, consumerTest);
-//DEFAULT_TEST (LogArchiverTest, heapTestReal);
-//DEFAULT_TEST (LogArchiverTest, fullPipelineTest);
-DEFAULT_TEST (LogArchiverTest, runScannerTest);
+DEFAULT_TEST (LogArchiverTest, consumerTest);
+DEFAULT_TEST (LogArchiverTest, heapTestReal);
+DEFAULT_TEST (LogArchiverTest, fullPipelineTest);
+DEFAULT_TEST (ArchiveScannerTest, runScannerTest);
+DEFAULT_TEST (ArchiveScannerTest, runMergerSeqTest);
+DEFAULT_TEST (ArchiveScannerTest, runMergerFullTest);
 
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
