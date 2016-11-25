0ad74baa76aef36d4cc423ebf4db1986c7690df6
Caetano Sauer <csauer@cs.uni-kl.de>
2016-11-25 14:37:23 +0100

    Moved w_findprime() into bf_tree.cpp

--- src/sm/bf_tree.cpp
+++ src/sm/bf_tree.cpp
@@ -13,7 +13,6 @@
 #include "smthread.h"
 #include "generic_page.h"
 #include <string.h>
-#include "w_findprime.h"
 #include <stdlib.h>
 
 #include "sm_base.h"
@@ -37,6 +36,50 @@
 
 // Template definitions
 #include "bf_hashtable.cpp"
+
+// lots of help from Wikipedia here!
+int64_t w_findprime(int64_t min)
+{
+    // the first 25 primes
+    static char const prime_start[] = {
+    // skip 2,3,5 because our mod60 test takes care of them for us
+    /*2, 3, 5,*/ 7, 11, 13, 17, 19, 23, 29, 31, 37, 41,
+    43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+    };
+    // x%60 isn't on this list, x is divisible by 2, 3 or 5. If it
+    // *is* on the list it still might not be prime
+    static char const sieve_start[] = {
+    // same as the start list, but adds 1,49 and removes 3,5
+    1, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 49, 53, 59
+    };
+
+    // use the starts to populate our data structures
+    std::vector<int64_t> primes(prime_start, prime_start+sizeof(prime_start));
+    char sieve[60];
+    memset(sieve, 0, sizeof(sieve));
+
+    for(uint64_t i=0; i < sizeof(sieve_start); i++)
+    sieve[int64_t(sieve_start[i])] = 1;
+
+    /* We aren't interested in 4000 digit numbers here, so a Sieve of
+       Erastothenes will work just fine, especially since we're
+       seeding it with the first 25 primes and avoiding the (many)
+       numbers that divide by 2,3 or 5.
+     */
+    for(int64_t x=primes.back()+1; primes.back() < min; x++) {
+    if(!sieve[x%60])
+        continue; // divides by 2, 3 or 5
+
+    bool prime = true;
+    for(int64_t i=0; prime && primes[i]*primes[i] <= x; i++)
+        prime = (x%primes[i]) > 0;
+
+    if(prime)
+        primes.push_back(x);
+    }
+
+    return primes.back();
+}
 
 ///////////////////////////////////   Initialization and Release BEGIN ///////////////////////////////////
 
