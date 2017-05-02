ed18c5128e74026c090c8cd3e948fdad3c702e05
Max Gilbert <m_gilbert13@cs.uni-kl.de>
2017-05-02 18:54:19 +0200

    Fixed the documentation of the page evictioners.

--- src/sm/page_evictioner.h
+++ src/sm/page_evictioner.h
@@ -35,15 +35,15 @@ public:
      * \brief   Constructor for page_evictioner_base
      * \details This instantiates a page evictioner that uses the RANDOM algorithm
      *          to select victims for replacement. It will serve the specified
-     *          \c bufferpool \c but won't use the specified \c options \c as this
+     *          \c bufferpool but won't use the specified \c options as this
      *          page replacement strategy doesn't need any further parameters.
      *
      * @param bufferpool The bf_tree_m the constructed page evictioner is used to
      *                   select pages for eviction for.
      * @param options    The options passed to the program on startup.
      */
-	page_evictioner_base(bf_tree_m* bufferpool, const sm_options& options);
-
+    page_evictioner_base(bf_tree_m* bufferpool, const sm_options& options);
+    
     /*!\fn      ~page_evictioner_base()
      * \brief   Destructor for page_evictioner_base
      * \details Destroys this instance.
@@ -132,18 +132,22 @@ public:
 
 
 protected:
-    /**
-     * The bufferpool for which this evictioner is responsible for.
+    /*!\var     _bufferpool
+     * \bried   The bufferpool for which this evictioner is responsible for
+     * \details This evictioner expects to be used for the eviction of pages from
+     *          the bufferpool referenced here.
      */
     bf_tree_m*                  _bufferpool;
     
-    /**
-     * Set if the \link _bufferpool \endlink uses pointer swizzling for page references.
+    /*!\var     _swizzling_enabled
+     * \brief   Pointer Swizzling in the bufferpool
+     * \details Set if the \link _bufferpool \endlink uses pointer swizzling for page references.
      */
     bool                        _swizzling_enabled;
     
-    /**
-     * Set if the parent's EMLSN should be updated during eviction.
+    /*!\var     _maintain_emlsn
+     * \brief   EMLSN-update by evictioner
+     * \details Set if the parent's EMLSN should be updated during eviction.
      */
     bool                        _maintain_emlsn;
     
@@ -155,7 +159,7 @@ protected:
      *          cached page). It acquires a LATCH_EX to prohibit the usage of the
      *          frame as the content of the buffer frame will definitely change.
      *
-     * @return The buffer frame that can be freed or \c 0 \c if no eviction victim could
+     * @return The buffer frame that can be freed or \c 0 if no eviction victim could
      *         be found.
      */
     virtual bf_idx          pick_victim();
@@ -168,48 +172,52 @@ protected:
      *
      * @param idx          The buffer frame that should be freed. If it can be freed,
      *                     it gets latched exclusively by this function.
-     * @param evicted_page Returns the page in the selected buffer frame \c idx \c.
-     * @return             Returns \c true \c if the page can be evicted and \c false \c
+     * @param evicted_page Returns the page in the selected buffer frame \c idx.
+     * @return             Returns \c true if the page can be evicted and \c false
      *                     if some property prevents the eviction of that page.
      */
     bool evict_page(bf_idx idx, PageID &evicted_page);
 
 
 private:
-	/**
-	 * When eviction is triggered, _about_ this number of buffer frames will be freed at
-	 * once. If this amount of frames is already free, the eviction does nothing and
-	 * goes back to sleep. Given as a ratio of the buffer size (currently 1%).
-	 */
-	const float EVICT_BATCH_RATIO = 0.01;
-
-	/**
-	 * Last buffer frame examined.
-	 */
-	bf_idx                      _current_frame;
+    /*!\var     EVICT_BATCH_RATIO
+     * \brief   Ratio of buffer frames freed as batch
+     * \details When eviction is triggered, _about_ this number of buffer frames will
+     *          be freed at once. If this amount of frames is already free, the
+     *          eviction does nothing and goes back to sleep. Given as a ratio of the
+     *          buffer size (currently 1%).
+     */
+    const float EVICT_BATCH_RATIO = 0.01;
     
-
+    /*!\var     _current_frame
+     * \brief   Last buffer frame examined
+     * \details The buffer frame index of the last frame that was checked by RANDOM
+     *          page replacement.
+     */
+    bf_idx                      _current_frame;
+    
+    
     /*!\fn      unswizzle_and_update_emlsn(bf_idx idx)
      * \brief   Unswizzles the pointer in the parent page and updates the EMLSN of that
      *          page
      * \details In case swizziling is enabled, it will unswizzle the parent point.
-	 *          Additionally, it will update the parent EMLSN.
-	 *          This two operations are kept in a single method because both require
-	 *          looking up the parent, latching, etc., so we save some work.
+     *          Additionally, it will update the parent EMLSN.
+     *          This two operations are kept in a single method because both require
+     *          looking up the parent, latching, etc., so we save some work.
      *
      * @param idx The buffer frame index where the page that gets evicted can be found.
-     * @return    \c true \c if the .
+     * @return    \c true if the .
      */
-	bool unswizzle_and_update_emlsn(bf_idx idx);
-
-	/*!\fn      do_work()
-	 * \brief   Function evicting pages in the eviciton thread
-	 * \details Runs in the eviction thread (executed when the eviction thread gets woken
-	 *          up and when terminated it terminates the eviction thread) and evicts pages
-	 *          as long as there are not that many buffer frames free as defined in
-	 *          \link EVICT_BATCH_RATIO \endlink.
-	 */
-	virtual void do_work();
+    bool unswizzle_and_update_emlsn(bf_idx idx);
+    
+    /*!\fn      do_work()
+     * \brief   Function evicting pages in the eviciton thread
+     * \details Runs in the eviction thread (executed when the eviction thread gets woken
+     *          up and when terminated it terminates the eviction thread) and evicts pages
+     *          as long as there are not that many buffer frames free as defined in
+     *          \link EVICT_BATCH_RATIO \endlink.
+     */
+    virtual void do_work();
 };
 
 /*!\class   page_evictioner_gclock
@@ -219,10 +227,10 @@ private:
  *          <A HREF="http://doi.org/10.1145/320263.320276">"Sequentiality and
  *          Prefetching in Database Systems"</A> by Alan Jay Smith.
  *          To use this page eviction algorithm, the only thing to do is
- *          to set the parameter \c sm_evict_policy \c to \c gclock \c when
- *          starting the \c zapps \c. To set the k-parameter (i in the original
- *          paper), the parameter \c sm_bufferpool_gclock_k \c is offered by
- *          \c zapps \c. The default value is 10.
+ *          to set the parameter \c sm_evict_policy to \c gclock when
+ *          starting the \c zapps. To set the k-parameter (i in the original
+ *          paper), the parameter \c sm_bufferpool_gclock_k is offered by
+ *          \c zapps. The default value is 10.
  *          On construction, this page evictioner needs to be connected to
  *          a bufferpool bf_tree_m for which this will serve. The bufferpool
  *          needs to call hit_ref(bf_idx idx) on every page hit and
@@ -237,8 +245,8 @@ public:
      * \brief   Constructor for page_evictioner_gclock
      * \details This instantiates a page evictioner that uses the GCLOCK algorithm
      *          to select victims for replacement. It will serve the specified
-     *          \c bufferpool \c and it will use the \c sm_bufferpool_gclock_k \c
-     *          parameter from the \c options \c to specify \link _k \endlink (default
+     *          \c bufferpool and it will use the \c sm_bufferpool_gclock_k
+     *          parameter from the \c options to specify \link _k \endlink (default
      *          value is 10).
      *          It also initializes the \link _counts \endlink array of referenced
      *          counters and it initializes the clock hand \link _current_frame \endlink
@@ -260,23 +268,23 @@ public:
     
     /*!\fn      hit_ref(bf_idx idx)
      * \brief   Updates the eviction statistics on page hit
-     * \details Sets the referenced counter of the specified buffer frame \c idx \c to
+     * \details Sets the referenced counter of the specified buffer frame \c idx to
      *          the value specified in \link _k \endlink.
      *
      * @param idx The frame of the \link _bufferpool \endlink that was fixed with a
      *            page hit.
      */
     virtual void            hit_ref(bf_idx idx);
-	
-	/*!\fn      unfix_ref(bf_idx idx)
-	 * \brief   Updates the eviction statistics on page unfix
-	 * \details Sets the referenced counter of the specified buffer frame \c idx \c to
+    
+    /*!\fn      unfix_ref(bf_idx idx)
+     * \brief   Updates the eviction statistics on page unfix
+     * \details Sets the referenced counter of the specified buffer frame \c idx to
      *          the value specified in \link _k \endlink as this page was still used
      *          until this point in time.
-	 *
-	 * @param idx The frame of the \link _bufferpool \endlink that was unfixed.
-	 */
-	virtual void            unfix_ref(bf_idx idx);
+     *
+     * @param idx The frame of the \link _bufferpool \endlink that was unfixed.
+     */
+    virtual void            unfix_ref(bf_idx idx);
     
     /*!\fn      miss_ref(bf_idx b_idx, PageID pid)
      * \brief   Updates the eviction statistics on page miss
@@ -361,7 +369,7 @@ public:
      */
     virtual void            unbuffered(bf_idx idx);
 
-    
+
 protected:
     /*!\fn      pick_victim()
      * \brief   Selects a page to be evicted from the \link _bufferpool \endlink
@@ -370,28 +378,33 @@ protected:
      *          cached page). It acquires a LATCH_EX to prohibit the usage of the
      *          frame as the content of the buffer frame will definitely change.
      *
-     * @return The buffer frame that can be freed or \c 0 \c if no eviction victim could
+     * @return The buffer frame that can be freed or \c 0 if no eviction victim could
      *         be found.
      */
     virtual bf_idx          pick_victim();
 
-    
+
 private:
-    /**
-     * The k-parameter (i in the original paper) of the algorithm. When a page is referenced,
-     * its referenced counter is set to this value.
+    /*!\var     _k
+     * \brief   k-parameter (set referenced counter to that value)
+     * \details The k-parameter (i in the original paper) of the algorithm. When a
+     *          page is referenced, its referenced counter is set to this value.
      */
     uint16_t            _k;
     
-    /**
-     * One referenced counter per buffer frame set to \link _k \endlink on page hits and
-     * decremented during the execution of \link pick_victim() \endlink.
+    /*!\var     _counts
+     * \brief   Referenced counters per buffer frame
+     * \details One referenced counter per buffer frame set to \link _k \endlink on
+     *          page hits and decremented during the execution of
+     *          \link pick_victim() \endlink.
      */
     uint16_t*           _counts;
     
-    /**
-     * Represents the clock hand pointing to the buffer frame that was checked last during the
-     * most recent execution of \link pick_victim() \endlink (evicted last).
+    /*!\var     _current_frame
+     * \brief   Clock hand into \link _counts \endlink
+     * \details Represents the clock hand pointing to the buffer frame that was
+     *          checked last during the most recent execution of
+     *          \link pick_victim() \endlink (evicted last).
      */
     bf_idx              _current_frame;
 };
